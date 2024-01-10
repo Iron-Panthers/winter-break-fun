1ad3f773ead51a47454db0f31b108e409ccb7a91
Samantha <27nguyens28@stu.smuhsd.org>
2024-01-10 08:28:46 -0800

    day one
    did half of array ;-; (got stuck a little w the second part but working on it)

--- lib/src/main/java/winterbreakstudying/Main.java
+++ lib/src/main/java/winterbreakstudying/Main.java
@@ -1,7 +1,11 @@
 package winterbreakstudying;
 
+import winterbreakstudying.ArrayPractice.ArrayPractice;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+
+    ArrayPractice.practice();
   }
 }
